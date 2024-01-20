50eb3d8511e65d4b43d65694eb8ed7a39af4c83a
amazingspidy <2018ds21112@gmail.com>
2024-01-20 15:00:27 +0900

    motion + tetris 합치기작업 진행중

--- Tetris/src/index.js
+++ Tetris/src/index.js
@@ -7,6 +7,162 @@ document.addEventListener("DOMContentLoaded", (event) => {
   canvas.height = 798;
 });
 
+
+
+async function setupWebcam() {
+  const video = document.getElementById('webcam');
+  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+  video.srcObject = stream;
+
+  return new Promise((resolve) => {
+    video.onloadedmetadata = () => {
+      resolve(video);
+    };
+  });
+}
+
+async function runPosenet() {
+  const net = await posenet.load();
+  const video = await setupWebcam();
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+
+  //이전 지점들과 각도를 저장하는 변수
+  let prevLeftAngle = 0;
+  let prevRightAngle = 0;
+  let prevLeftWristX = 0;
+  let prevRightWristX = 0;
+  let squareElement = document.getElementById('square');
+  //현재 도형의 회전된각
+  let rotateDegree = 0;
+  
+
+
+  setInterval(async () => {
+    const pose = await net.estimateSinglePose(video, {
+      flipHorizontal: false,
+      decodingMethod: 'single-person'
+    });
+
+    // 캔버스를 지우고 새로 그립니다.
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    pose.keypoints.forEach(keypoint => {
+      if (['leftShoulder', 'rightShoulder', 'leftElbow', 'rightElbow', 'leftWrist', 'rightWrist'].includes(keypoint.part)) {
+        ctx.beginPath();
+        ctx.arc(keypoint.position.x, keypoint.position.y, 5, 0, 2 * Math.PI);
+        ctx.fillStyle = 'red';
+        ctx.fill();
+      }
+    });
+
+    // shoulder, elbow, wrist의 위치를 찾고, 내적을통해 사이각을 구함.
+    // 왼쪽 각도 계산산
+    let leftShoulder = pose.keypoints.find(keypoint => keypoint.part === 'leftShoulder').position;
+    let leftElbow = pose.keypoints.find(keypoint => keypoint.part === 'leftElbow').position;
+    let leftWrist = pose.keypoints.find(keypoint => keypoint.part === 'leftWrist').position;
+
+    let vectorA = { x: leftShoulder.x - leftElbow.x, y: leftShoulder.y - leftElbow.y };
+    let vectorB = { x: leftWrist.x - leftElbow.x, y: leftWrist.y - leftElbow.y };
+
+    let leftDotProduct = vectorA.x * vectorB.x + vectorA.y * vectorB.y;
+    let leftMagnitudeA = Math.sqrt(vectorA.x * vectorA.x + vectorA.y * vectorA.y);
+    let leftMagnitudeB = Math.sqrt(vectorB.x * vectorB.x + vectorB.y * vectorB.y);
+
+    let leftAngleInRadians = Math.acos(leftDotProduct / (leftMagnitudeA * leftMagnitudeB));
+    let leftAngleInDegrees = leftAngleInRadians * (180 / Math.PI);
+
+    let leftWristX = leftWrist.x;
+
+    // 오른쪽 각도 계산
+    let rightShoulder = pose.keypoints.find(keypoint => keypoint.part === 'rightShoulder').position;
+    let rightElbow = pose.keypoints.find(keypoint => keypoint.part === 'rightElbow').position;
+    let rightWrist = pose.keypoints.find(keypoint => keypoint.part === 'rightWrist').position;
+
+    vectorA = { x: rightShoulder.x - rightElbow.x, y: rightShoulder.y - rightElbow.y };
+    vectorB = { x: rightWrist.x - rightElbow.x, y: rightWrist.y - rightElbow.y };
+
+    let rightDotProduct = vectorA.x * vectorB.x + vectorA.y * vectorB.y;
+    let rightMagnitudeA = Math.sqrt(vectorA.x * vectorA.x + vectorA.y * vectorA.y);
+    let rightMagnitudeB = Math.sqrt(vectorB.x * vectorB.x + vectorB.y * vectorB.y);
+
+    let rightAngleInRadians = Math.acos(rightDotProduct / (rightMagnitudeA * rightMagnitudeB));
+    let rightAngleInDegrees = rightAngleInRadians * (180 / Math.PI);
+
+    let rightWristX = rightWrist.x;
+
+    //각도의 변화값. (이전 각도와의 차이)
+    let leftAngleDelta = leftAngleInDegrees - prevLeftAngle;
+    let rightAngleDelta = rightAngleInDegrees - prevRightAngle;
+
+    let eventElement = document.getElementById('event');
+
+    if (leftAngleDelta > rightAngleDelta) {
+      if (leftAngleDelta > 35 && leftAngleInDegrees > prevLeftAngle && leftWristX > prevLeftWristX) {
+        eventElement.textContent = '왼쪽 회전';
+        // rotateDegree -= 30;  // 30도 좌측회전
+        rotateCompound(activeBlock, -Math.PI / 12); // 숫자 늘릴수록 적게 회전전
+      }
+      else {
+        eventElement.textContent = '유지';
+      }
+    }
+
+
+    else {
+      if (rightAngleDelta > 35 && rightAngleInDegrees > prevRightAngle && rightWristX < prevRightWristX) {
+        eventElement.textContent = '오른쪽 회전';
+        //rotateDegree += 30;  // 30도 우측회전
+        rotateCompound(activeBlock, Math.PI / 12);
+
+      }
+      else {
+        eventElement.textContent = '유지';
+      }
+    }
+
+    // // 블록은 코를 인식하여 따라갑니다.
+    // let nose = pose.keypoints.find(keypoint => keypoint.part === 'nose').position;
+
+    // let noseXFlipped = 640 - nose.x;  //좌우반전으로 인함.
+    // //let noseY = nose.y; 
+    // squareElement.style.left = `${noseXFlipped}px`;
+    // //squareElement.style.top = `${noseY}px`;
+
+    // squareY += 30;  // 블록이 천천히 내려가도록 y좌표를 증가시킵니다.
+    // if (squareY > 640) {  // 만약 블록이 화면 밖으로 벗어나면
+    //   squareY = 0;  // 다시 화면 맨 위로 되돌립니다.
+    // }
+    // squareElement.style.top = `${squareY}px`;
+
+    // squareElement.style.transform = `rotate(${rotateDegree}deg)`;
+
+    
+    
+    // 현재 각도와 wrist x좌표를 이전 값으로 저장
+    prevLeftAngle = leftAngleInDegrees;
+    prevRightAngle = rightAngleInDegrees;
+    prevRightWristX = rightWristX;
+    prevLeftWristX = leftWristX;
+
+    let leftAngleElement = document.getElementById('leftAngle');
+    leftAngleElement.textContent = `Left Angle: ${leftAngleInDegrees.toFixed(2)} degrees`;
+
+    let rightAngleElement = document.getElementById('rightAngle');
+    rightAngleElement.textContent = `Right Angle: ${rightAngleInDegrees.toFixed(2)} degrees`;
+
+    let leftWristXElement = document.getElementById('leftWristX');
+    leftWristXElement.textContent = `Left Wrist X: ${leftWrist.x.toFixed(2)}`;
+
+    let rightWristXElement = document.getElementById('rightWristX');
+    rightWristXElement.textContent = `Right Wrist X: ${rightWrist.x.toFixed(2)}`;
+
+  }, 250);
+}
+runPosenet();
+
+
+
 const mapWidth = 392;
 const mapHeight = 798;
 const blockSize = 32;
@@ -36,8 +192,8 @@ Physics(function (world) {
   world.add(
     Physics.behavior("edge-collision-detection", {
       aabb: Physics.aabb(0, 0, mapWidth, mapHeight),
-      restitution: 0.2,
-      cof: 0.8,
+      restitution: 0.0000002,
+      cof: 0.000008,
     })
   );
   // 일정 시간마다 블록 생성
@@ -111,15 +267,19 @@ Physics(function (world) {
         height: 10,
         treatment: "static",
         label: "천장",
+        restitution: 0.3, // 반발력 감소
+        cof: 0.3, // 마찰 계수 감소
       }),
       Physics.body("rectangle", {
         // 바닥
         x: mapWidth / 2,
-        y: mapHeight + 64,
+        y: mapHeight,
         width: mapWidth - 16,
         height: 10,
         treatment: "static",
         label: "바닥",
+        restitution: 0.3, // 반발력 감소
+        cof: 0.3, // 마찰 계수 감소
       }),
       Physics.body("rectangle", {
         // 좌측 벽
@@ -129,6 +289,8 @@ Physics(function (world) {
         height: mapHeight,
         treatment: "static",
         label: "좌측 벽",
+        restitution: 0.3, // 반발력 감소
+        cof: 0.3, // 마찰 계수 감소
       }),
       Physics.body("rectangle", {
         // 우측 벽
@@ -138,6 +300,8 @@ Physics(function (world) {
         height: mapHeight,
         treatment: "static",
         label: "우측 벽",
+        restitution: 0.3, // 반발력 감소
+        cof: 0.3, // 마찰 계수 감소
       }),
     ];
     walls.forEach((wall) => world.add(wall));
@@ -160,8 +324,8 @@ Physics(function (world) {
         width: blockSize,
         height: blockSize,
         treatment: "dynamic",
-        restitution: 0.3, // 반발력 감소
-        cof: 0.3, // 마찰 계수 감소
+        restitution: 0.00001, // 반발력 감소
+        cof: 0.000001, // 마찰 계수 감소
       });
       blockParts.push(part);
     });
@@ -203,6 +367,15 @@ Physics(function (world) {
   world.on("collisions:detected", function (data) {
     const collisions = data.collisions;
     collisions.forEach(function (collision) {
+
+      // 충돌한 두 객체
+      const bodyA = collision.bodyA;
+      const bodyB = collision.bodyB;
+
+      // 충돌 후 속도 감소
+      bodyA.state.vel.mult(0.6);
+      bodyB.state.vel.mult(0.6);
+
       if (collision.bodyA === activeBlock || collision.bodyB === activeBlock) {
         const otherBody =
           collision.bodyA === activeBlock ? collision.bodyB : collision.bodyA;
@@ -283,3 +456,5 @@ Physics(function (world) {
     Physics.util.ticker.start();
   });
 });
+
+
