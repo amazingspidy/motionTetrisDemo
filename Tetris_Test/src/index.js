80033244c3b096f97050169f71ffef0c9549c6aa
Sleeeeeepy <adjustedmin@gmail.com>
2024-01-26 13:14:18 +0000

    remove line complete.

--- Tetris_Test/src/index.js
+++ Tetris_Test/src/index.js
@@ -1,7 +1,15 @@
 const Matter = require("matter-js");
 const Cutter = require("martinez-polygon-clipping");
-const line = [[[1000, 300], [1000, 360], [-1000, 360], [-1000, 300], [1000, 300]]];
-
+const UpperBound = 330;
+const LowerBound = 300;
+const line = [[[10000, LowerBound], [10000, UpperBound], [-10000, UpperBound], [-10000, LowerBound], [10000, LowerBound]]];
+const lineVertices = [
+    { x: 10000, y: 0 },
+    { x: 10000, y: UpperBound - LowerBound },
+    { x: -10000, y: UpperBound - LowerBound },
+    { x: -10000, y: 0 }
+];
+  
 const Engine = Matter.Engine,
 Render = Matter.Render,
 World = Matter.World,
@@ -13,98 +21,89 @@ Composite = Matter.Composite;
 const engine = Engine.create();
 const render = Render.create({
 element: document.body,
-engine: engine
+engine: engine,
+options: {
+    wireframes: false
+}
 });
 
-let OriginalPolygonRemoved = false;
-const blockSize = 80;
 Render.run(render);
-
+const nonCollidableLine = Matter.Bodies.fromVertices(0, LowerBound + (UpperBound - LowerBound) / 2, [lineVertices], {
+    isStatic: true, // Make the body static (not affected by physics)
+    collisionFilter: {
+      category: 0x0000, // Set a category that does not collide with any other category
+      mask: 0x0000 // Set a mask that does not collide with any other category
+    },
+    render: {
+        fillStyle: 'purple'
+    },
+    label: "line"
+  });
+Matter.World.add(engine.world, nonCollidableLine);
 const mouse = Matter.Mouse.create(render.canvas),
 mouseConstraint = Matter.MouseConstraint.create(engine, {
     mouse: mouse
 });
-
+document.addEventListener('keydown', (e) => {
+    const key = e.key;
+    console.log(key);
+    switch (key) {
+        case 'c':
+            let bodies = Composite.allBodies(engine.world).filter((body) => {
+                if (body.label === "line") {
+                    return false;
+                } else if (body.label === "wall") {
+                    return false;
+                }
+                return true;
+            })
+            
+            bodies.forEach((body) => {
+                removeLines(body);
+            })
+        
+            console.log("len:", Composite.allBodies(engine.world).length);
+    }
+});
 World.add(engine.world, mouseConstraint);
 // 여러 폴리곤을 가진 복합체 생성
 
 const compoundBody = Matter.Body.create({
     parts: [
-        Matter.Bodies.rectangle(100, 100, 50, 50),
-        Matter.Bodies.rectangle(150, 100, 50, 50),
-        Matter.Bodies.rectangle(125, 50, 50, 50),
+        Matter.Bodies.rectangle(100, 100, 50, 50, {
+            render: {
+                fillStyle: 'blue'
+            }
+        }),
+        Matter.Bodies.rectangle(150, 100, 50, 50, {
+            render: {
+                fillStyle: 'red'
+            }
+        }),
+        Matter.Bodies.rectangle(200, 100, 50, 50, {
+            render: {
+                fillStyle: 'yellow'
+            }
+        }),
+        Matter.Bodies.rectangle(150, 50, 50, 50, {
+            render: {
+                fillStyle: 'green'
+            }
+        }),
     ],
 });
 
 // 벽 생성
-const wall = Bodies.rectangle(400, 400, 800, 20, {
+const wall = Bodies.rectangle(400, 400, 2000, 20, {
 isStatic: true,
 render: {
     fillStyle: 'gray'
-}
+},
+label: 'wall'
 });
 
 // 복합체와 벽을 세계에 추가
 World.add(engine.world, [compoundBody, wall]);
-
-// 마우스 클릭 이벤트
-Matter.Events.on(mouseConstraint, 'mousedown', (event) => {
-
-if (OriginalPolygonRemoved) {
-    return;
-}
-World.remove(engine.world, compoundBody);
-OriginalPolygonRemoved = true;
-const bodyToAdd = [];
-for (let i = 1; i < compoundBody.parts.length; i++) {
-    const body = compoundBody.parts[i];
-    const poly = verticesToGeometry(body);
-    const cut = Cutter.diff(poly, line);
-    console.log(cut);
-    const cut1 = cut[0];
-    const cut2 = cut[1];
-
-    if (cut1) {
-        bodyToAdd.push(createBody(cut1));
-    }
-
-    if (cut2) {
-        bodyToAdd.push(createBody(cut2));
-    }
-}
-
-let unionFind = new UnionFind(bodyToAdd);
-
-for (let i = 0; i < bodyToAdd.length; i++) {
-    for (let j = i + 1; j < bodyToAdd.length; j++) {
-        let result = shouldCombine(bodyToAdd[i].vertices, bodyToAdd[j].vertices, 1);
-        if (result) {
-            unionFind.union(i, j);
-        }
-    }
-}
-
-const group = new Map();
-for (let i = 0; i < bodyToAdd.length; i++) {
-    let root = unionFind.find(i);
-    if (group.get(root)) {
-        group.get(root).push(bodyToAdd[i]);
-    } else {
-        group.set(root, []);
-        group.get(root).push(bodyToAdd[i]);
-    }
-}
-const realBodyToAdd = [];
-console.log(group);
-group.forEach((value) => {
-    console.log("val", value);
-    let add = Matter.Body.create({
-        parts: value
-    });
-    realBodyToAdd.push(add);
-})
-World.add(engine.world, realBodyToAdd);
-});
 
 class UnionFind {
     constructor(elements) {
@@ -142,11 +141,10 @@ class UnionFind {
     }
 }
 
-function createBody(geometry) {
+function createBody(geometry, style) {
     const points = geoJsonToVectors(geometry);
-    console.log(points, "point");
     return Bodies.fromVertices(Vertices.centre(points).x, Vertices.centre(points).y, points, {
-        render: {fillStyle: 'blue'}
+        render: {fillStyle: style}
     });
 }
 
@@ -204,4 +202,57 @@ function shouldCombine(body1, body2, maxDistance) {
     return false;
 }
 
+function removeLines(body) {
+    World.remove(engine.world, body);
+    const bodyToAdd = [];
+    console.log(body);
+    for (let i = 1; i < body.parts.length; i++) {
+        const part = body.parts[i];
+        const poly = verticesToGeometry(part);
+        const cut = Cutter.diff(poly, line);
+        const cut1 = cut[0];
+        const cut2 = cut[1];
+
+        if (cut1) {
+            bodyToAdd.push(createBody(cut1, body.parts[i].render.fillStyle));
+        }
+
+        if (cut2) {
+            bodyToAdd.push(createBody(cut2, body.parts[i].render.fillStyle));
+        }
+    }
+
+    let unionFind = new UnionFind(bodyToAdd);
+    for (let i = 0; i < bodyToAdd.length; i++) {
+        for (let j = i + 1; j < bodyToAdd.length; j++) {
+            let result = shouldCombine(bodyToAdd[i].vertices, bodyToAdd[j].vertices, 1);
+            if (result) {
+                unionFind.union(i, j);
+            }
+        }
+    }
+
+    const group = new Map();
+    for (let i = 0; i < bodyToAdd.length; i++) {
+        let root = unionFind.find(i);
+        if (group.get(root)) {
+            group.get(root).push(bodyToAdd[i]);
+        } else {
+            group.set(root, []);
+            group.get(root).push(bodyToAdd[i]);
+        }
+    }
+    const realBodyToAdd = [];
+    console.log(group);
+    group.forEach((value) => {
+        console.log("val", value);
+        let add = Matter.Body.create({
+            parts: value
+        });
+        console.log("added:", add.id, value);
+        realBodyToAdd.push(add);
+    })
+
+    World.add(engine.world, realBodyToAdd);
+}
 Matter.Runner.run(engine);
